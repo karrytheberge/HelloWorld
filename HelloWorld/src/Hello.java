7e371bc5096be77bfdaf7f514c0f0277bbaec0a9
Karry Theberge <karry.rogers@maine.edu>
2017-01-22 16:50:21 -0500

    Tutorial

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+	
+	/** Implement the application
+	*@param args arguments 
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
+1
