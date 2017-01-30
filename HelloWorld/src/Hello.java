7f4660a7edd2b5953d04c5d9bfff3d14c6ced42d
Karry Theberge <karry.rogers@maine.edu>
2017-01-30 16:38:20 -0500

    Second tutorial change

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Here Ye, Here Ye: Important Announcement");
 		System.out.println("Hello world!");
+		System.out.println("Now...goodbye");
 	}
 
 }
